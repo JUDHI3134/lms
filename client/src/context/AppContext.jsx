3783adbfd3011015d0e38f6bcd778f65cae2f24d
JUDHI3134 <judhistirbehera241@gmail.com>
2025-01-26 21:49:23 +0530

    create courses section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,15 +1,41 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
     
+    const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
 
-    const value = {
+    const [allCourses, setAllCourses] = useState([])
 
+    //fetch all courses
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
     }
 
+    //function to calculate avarage rating of courses
+    const calculateRating = (course) => {
+        if (course.courseRatings.length === 0) {
+            return 0;
+        }
+        let totalRating = 0
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length
+    }
+
+    const value = {
+        currency, allCourses, navigate, calculateRating
+    }
+
+    useEffect(() => {
+        fetchAllCourses();
+    },[])
 
     return (
         <AppContext.Provider value={value}>
