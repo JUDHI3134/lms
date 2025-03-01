42004b190b2e7f1a84200e445ba7e487af059359
JUDHI3134 <judhistirbehera456@gmail.com>
2025-03-01 22:09:00 +0530

    api controller function to manage clerk user with database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 //initialized express
 const app = express();
@@ -12,10 +13,12 @@ await connectDB();
 //middleware
 app.use(cors())
 
+
 //route
 app.get("/", (req, res) => {
     res.send("Api Working")
 })
+app.post("/clerk",express.json(), clerkWebhooks)
 
 //port
 const PORT = process.env.PORT || 5000
