252b899575377f5b7294b07cda7c910d7325c7d3
JUDHI3134 <judhistirbehera456@gmail.com>
2025-03-01 22:45:41 +0530

    route fo update role

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import "dotenv/config"
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 //initialized express
 const app = express();
@@ -12,13 +14,15 @@ await connectDB();
 
 //middleware
 app.use(cors())
+app.use(clerkMiddleware())
 
 
 //route
 app.get("/", (req, res) => {
     res.send("Api Working")
 })
-app.post("/clerk",express.json(), clerkWebhooks)
+app.post("/clerk", express.json(), clerkWebhooks)
+app.use("/api/educator", express.json(), educatorRouter)
 
 //port
 const PORT = process.env.PORT || 5000
