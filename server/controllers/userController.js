55fc2246efb03ad8ef38b935e4a3d4f483c1df52
JUDHI3134 <judhistirbehera456@gmail.com>
2025-03-02 11:07:39 +0530

    user enrolled courses with lecture links endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,3 +17,16 @@ export const getUserData = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+//user enrolled courses with lecture links
+export const userEnrolledCourses = async (req, res) => {
+    try {
+        const userId = req.auth.userId
+        const userData = await User.findById(userId).populate('enrolledCourses')
+
+        res.json({success: true, enrolledCourses:userData.enrolledCourses})
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
